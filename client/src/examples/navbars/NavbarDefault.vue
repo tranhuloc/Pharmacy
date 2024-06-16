5d00c695b1d4d7ea76b5dad2a89957f1360445c6
TranHuuLoc <tran.huu.loc.17082k2@gmail.com>
2024-06-16 16:44:26 +0700

    change navbar

--- client/src/examples/navbars/NavbarDefault.vue
+++ client/src/examples/navbars/NavbarDefault.vue
@@ -10,6 +10,7 @@ import ArrDark from "@/assets/img/down-arrow-dark.svg";
 import DownArrWhite from "@/assets/img/down-arrow-white.svg";
 import { ShoppingCart } from '@element-plus/icons-vue'
 import { useStore } from 'vuex';
+import downArrow from "@/assets/img/down-arrow.svg";
 
 /**
  * Variable define
@@ -172,35 +173,50 @@ const handleLogout = async () => {
               <img :src="getArrowColor()" alt="down-arrow" class="arrow ms-2 d-lg-block d-none" />
               <img :src="getArrowColor()" alt="down-arrow" class="arrow ms-1 d-lg-none d-block ms-auto" />
             </a>
-            <div class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3"
-              aria-labelledby="dropdownMenuCategory">
-              <div class="row d-none d-lg-block">
-                <div class="col-12 px-4 py-2">
-                  <div class="row">
-                    <div class="position-relative">
-                      <div v-for="item in data">
-                        <div class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1">
-                          {{ item.category.category_name }}
+            <div
+              class="dropdown-menu dropdown-menu-end dropdown-menu-animation dropdown-md dropdown-md-responsive p-3 border-radius-lg mt-0 mt-lg-3"
+              aria-labelledby="dropdownMenuBlocks">
+              <div class="d-none d-lg-block">
+                <ul class="list-group">
+                  <li v-for="item in data"
+                    class="nav-item dropdown dropdown-hover dropdown-subitem list-group-item border-0 p-0">
+                    <a class="dropdown-item py-2 ps-3 border-radius-md" href="javascript:;">
+                      <div class="d-flex">
+                        <div class="w-100 d-flex align-items-center justify-content-between">
+                          <div>
+                            <h6
+                              class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center p-0">
+                              {{ item.category.category_name }}
+                            </h6>
+                          </div>
+                          <img :src="downArrow" alt="down-arrow" class="arrow" />
                         </div>
-                        <RouterLink v-for="subcategory in item.subcategories"
-                          :to="{ name: 'page-collection', params: { name: subcategory.subcategory_name } }"
-                          class="dropdown-item border-radius-md">
-                          <span>{{ subcategory.subcategory_name }}</span>
-                        </RouterLink>
+                      </div>
+                    </a>
+                    <div class="dropdown-menu mt-0 py-3 px-2 mt-3">
+                      <RouterLink :to="{ name: 'page-collection', params: { name: subcategory.subcategory_name } }"
+                        v-for="subcategory in item.subcategories" class="dropdown-item ps-3 border-radius-md mb-1">
+                        {{ subcategory.subcategory_name }}
+                      </RouterLink>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="row d-lg-none">
+                <div v-for="item in data" class="col-md-12">
+                  <div class="d-flex mb-2">
+                    <div class="w-100 d-flex align-items-center justify-content-between">
+                      <div>
+                        <h6
+                          class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center p-0">
+                          {{ item.category.category_name }}
+                        </h6>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div class="d-lg-none">
-                <div v-for="item in data">
-                  <div class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1">
-                    {{ item.category.category_name }}
-                  </div>
-                  <RouterLink v-for="subcategory in item.subcategories"
-                    :to="{ name: 'page-collection', params: { name: subcategory.subcategory_name } }"
-                    class="dropdown-item border-radius-md">
-                    <span>{{ subcategory.subcategory_name }}</span>
+                  <RouterLink :to="{ name: 'page-collection', params: { name: subcategory.subcategory_name } }"
+                    v-for="subcategory in item.subcategories" class="dropdown-item ps-3 border-radius-md mb-1">
+                    {{ subcategory.subcategory_name }}
                   </RouterLink>
                 </div>
               </div>
@@ -209,6 +225,7 @@ const handleLogout = async () => {
           <li v-if="isLogged" class="nav-item dropdown dropdown-hover mx-2">
             <a role="button" class="nav-link ps-2 d-flex cursor-pointer align-items-center" :class="getTextColor()"
               id="dropdownAccount" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="material-icons opacity-6 me-2 text-md" :class="getTextColor()">person</i>
               {{ userInfo.fullname }}
             </a>
             <div class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3"
